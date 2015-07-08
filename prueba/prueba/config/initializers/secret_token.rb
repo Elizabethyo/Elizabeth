cb18fc07f77c2367432646e9dd146f43d6adc4c2
Elizabeth <eli-1888-@hotmail.com>
2015-07-08 18:43:58 -0300

    Proyecto Inicial

---
+++ prueba/prueba/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prueba::Application.config.secret_token = 'a2a27562480d44da08739fe02b25c1de2b7ff84468fccd7fa51b8a5188f75c00e0c8db180df20136e7c884e83701d6c6d8232ee0d764c69fe8b3f4129cf1cccf'
